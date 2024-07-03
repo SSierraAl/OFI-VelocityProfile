d168f13499fe884f5226646fad96719df8fa17aa
Sebastián Sierra <42323058+SebasSA1@users.noreply.github.com>
2024-07-03 12:51:14 +0200

    Update Main Branch

--- GlobalGUI/main.py
+++ GlobalGUI/main.py
@@ -29,6 +29,7 @@ from TAB_Home_Read import *
 from DAQ_Reader_Global import *
 from TAB_Scanning import *
 
+
 import pyqtgraph as pg
 import sys
 from PySide6.QtCore import QTimer
@@ -39,6 +40,8 @@ import subprocess as sp
 # MAIN WINDOW
 # ///////////////////////////////////////////////////////////////
 class MainWindow(QMainWindow):
+    """ Class for setting up window GUI interface
+    """
     def __init__(self):
         super().__init__()
 
@@ -65,7 +68,7 @@ class MainWindow(QMainWindow):
         # SET DAQ ACQUISIITON
         # ///////////////////////////////////////////////////////////////
         Set_DAQ_Functions(self)
-        
+
 
         # SET HOME TAB
         # ///////////////////////////////////////////////////////////////
@@ -73,14 +76,13 @@ class MainWindow(QMainWindow):
 
         # SET SCANNING TAB
         # ///////////////////////////////////////////////////////////////
-        Set_Scanning_Tab(self)
-
+        self.scan_functions_instance = Scan_functions(self)
+        Set_Scanning_Tab(self, self.scan_functions_instance) #TODO: the scan_functions_instance of self can be used within the set_scanning_tab now, without having to pass it as an argument.
 
         # ///////////////////////////////////////////////////////////////
         # SHOW MAIN WINDOW
         # ///////////////////////////////////////////////////////////////
         self.show()
-
     
 
 
@@ -99,20 +101,19 @@ class MainWindow(QMainWindow):
         # ///////////////////////////////////////////////////////////////
         
         # HOME BTN
-        if btn.objectName() == "btn_home":
+        if btn.objectName() == "btn_home": #display home page
             # Select Menu
             self.ui.left_menu.select_only_one(btn.objectName())
             MainFunctions.set_page(self, self.ui.load_pages.page_1)
 
 
-        # HOME BTN
+        # CALIBRATION BTN
         if btn.objectName() == "btn_search":
             # Select Menu
             self.ui.left_menu.select_only_one(btn.objectName())
             MainFunctions.set_page(self, self.ui.load_pages.page_calib)
 
-
-        # ZABER CONECTION
+        # ZABER CONECTION BTN
         # ///////////////////////////////////////////////////////////////
         if btn.objectName() == "btn_zaber":
             # Select Menu
